bb25c0e90c373b1859ea6316f364d20588274d3c
BigEgg <cd_bigegg@hotmail.com>
2018-11-17 15:23:41 -0800

    Add the logic for other assignment part

--- Assignment6/Neural Networks/StartingPoint5.py
+++ Assignment6/Neural Networks/StartingPoint5.py
@@ -74,3 +74,69 @@ if __name__=="__main__":
     plt.legend()
     print("Close the plot diagram to continue program")
     plt.show()
+
+    ############################
+
+    print('========== Visualize the Weights ==========')
+    model = NeuralNetworks.NeuralNetworks(len(xTrain[0]), [ 2 ], 1)
+    print('Training the model')
+    for i in range(200):
+        model.fit_one(xTrain, yTrain, 10, 0.05)
+    hidden_layer_weights = model.weights[0]
+    print('Output the weights')
+    # for i in range(len(hidden_layer_weights)):
+    #     weights = hidden_layer_weights[i]
+    #     print(weights)
+    Assignment5Support.VisualizeWeights(hidden_layer_weights[0], './weight-0.jpg')
+    Assignment5Support.VisualizeWeights(hidden_layer_weights[1], './weight-1.jpg')
+
+    ############################
+
+    print('========== Find Underfitting and Overfiting ==========')
+    fig, ax = plt.subplots()
+    ax.grid(True)
+    x = []
+    for i in range(200):
+        x.append(i)
+
+    print('Build Neural Network with 1 hidden layer, and 15 nodes for each layer')
+    model = NeuralNetworks.NeuralNetworks(len(xTrain[0]), [ 2 ], 1)
+
+    y = []
+    y_test = []
+    y_accuracy = []
+    y_test_accuracy = []
+    for i in range(200):
+        model.fit_one(xTrain, yTrain, 10, 0.05)
+
+        y.append(model.loss(xTrain, yTrain))
+        y_test.append(model.loss(xTest, yTest))
+
+        predicted = model.predict(xTrain)
+        accuracy = EvaluationsStub.Accuracy(yTrain, predicted)
+        y_accuracy.append(accuracy)
+        predicted = model.predict(xTest)
+        testAccuracy = EvaluationsStub.Accuracy(yTest, predicted)
+        y_test_accuracy.append(testAccuracy)
+
+    plt.plot(x, y, label = 'Training Set Loss')
+    plt.plot(x, y_test, label = 'Test Set Loss')
+
+    print('### Plot Loss ###')
+    plt.xlabel('Iterations')
+    plt.ylabel('Loss')
+    plt.title('Neural Networks\'s Loss on Training and Test Set')
+    plt.legend()
+    print("Close the plot diagram to continue program")
+    plt.show()
+
+    plt.plot(x, y_accuracy, label = 'Training Set Accuracy')
+    plt.plot(x, y_test_accuracy, label = 'Test Set Accuracy')
+
+    print('### Plot Accuracy ###')
+    plt.xlabel('Iterations')
+    plt.ylabel('Accuracy')
+    plt.title('Neural Networks\'s Accuracy vs Iterations')
+    plt.legend()
+    print("Close the plot diagram to continue program")
+    plt.show()
